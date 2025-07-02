187b1c0e8181817507e8e8fdb0ee8aa66bc8c01b
Haruto K <aoi.stella.dev@gmail.com>
2025-07-03 01:10:57 +0900

    modify: moved folder

--- functions/src/repository/CreatorRepository/CreatorRepository.ts
+++ functions/src/repository/CreatorRepository/CreatorRepository.ts
@@ -1,5 +1,5 @@
-import {Creator, CreatorDocument} from "../models/creator";
-import {db} from "../config/firebase";
+import {Creator, CreatorDocument} from "../../models/creator";
+import {db} from "../../config/firebase";
 import type {Query} from "firebase-admin/firestore";
 
 /**
