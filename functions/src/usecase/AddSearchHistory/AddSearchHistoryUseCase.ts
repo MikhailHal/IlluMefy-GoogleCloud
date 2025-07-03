e5489f988afd581ea30e8ea393700ae557c187e2
Haruto K <aoi.stella.dev@gmail.com>
2025-07-03 22:29:36 +0900

    feat: implement History UseCases (AddSearchHistory, AddViewHistory)

---
+++ functions/src/usecase/AddSearchHistory/AddSearchHistoryUseCase.ts
@@ -0,0 +1,26 @@
+import { UserRepository } from "../../repository/UserRepository/UserRepository";
+
+/**
+ * 検索履歴追加ユースケース
+ */
+export class AddSearchHistoryUseCase {
+    /**
+     * コンストラクタ
+     *
+     * @param {UserRepository} userRepository ユーザーリポジトリ
+     */
+    constructor(
+        private userRepository: UserRepository
+    ) {}
+
+    /**
+     * 検索履歴追加処理
+     *
+     * @param {string} userId ユーザーID
+     * @param {string[]} tagIds 検索に使用したタグIDの配列
+     * @return {void}
+     */
+    public async execute(userId: string, tagIds: string[]): Promise<void> {
+        return await this.userRepository.addSearchHistory(userId, tagIds);
+    }
+}
