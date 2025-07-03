e57b171683955df6f4993eb93cc93da96a542f52
Haruto K <aoi.stella.dev@gmail.com>
2025-07-03 22:20:27 +0900

    feat: implement GetPopularCreatorsUseCase with DI

---
+++ functions/src/usecase/GetPopularCreator/GetPopularCreatorsUseCase.ts
@@ -0,0 +1,27 @@
+import {Creator} from "../../models/creator";
+import {CreatorRepository} from "../../repository/CreatorRepository/CreatorRepository";
+
+/**
+ * 人気クリエイター取得ユースケース
+ */
+export class GetPopularCreatorsUseCase {
+    /**
+     * コンストラクタ
+     *
+     * @param {CreatorRepository} creatorRepository クリエイターリポジトリ
+     */
+    constructor(
+        private creatorRepository: CreatorRepository,
+    ) {}
+
+    /**
+     * 人気クリエイター取得処理
+     *
+     * @param {number} fetchCount 取得件数
+     * @return {Creator[]} 人気クリエイター
+     */
+    public async getPopularCreators(fetchCount: number): Promise<Creator[]> {
+        let creators = await this.creatorRepository.getPopularCreators(fetchCount);
+        return creators
+    }
+}
